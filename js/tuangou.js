172d7203641f6033d9e28fec6a380fca4e677ae4
YuanChaobo <yuanchaob@163.com>
2017-05-07 18:22:33 +0800

    tuangou07

--- js/tuangou.js
+++ js/tuangou.js
@@ -1,4 +1,23 @@
 $(function(){
+
+
+	// 健身nav
+	$('#nav').find('.nav-list').hover(
+		function(){
+			$(this).siblings('li').find('i').removeClass('sanjiao');
+			$(this).find('i').addClass('sanjiao');
+			$(this).find('.list-div').css({
+				'display':'block',
+			});
+			// .parents('.nav-list').siblings('.nav-list').find('.list-div').css({
+			// 	'display':'none',
+			// })
+		},function(){
+			$(this).find('i').removeClass('sanjiao');
+			$(this).find('.list-div').css('display','none');
+			$(this).parents('.nav-f-ul').find('.nav-list').eq(1).find('i').addClass('sanjiao').parents('.nav-list').find('.list-div').addClass('block');
+		}
+	)
 	// 团购分类hover
 	$('#categories').find('.tg-dropmenu-li').hover(
 		function(e){
