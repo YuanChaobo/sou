6d83e839ca887e65fe3743a9f96648c18adf8dad
yuanchao <yuanchaob@163.com>
2017-05-30 16:23:11 +0800

    2017.5.30

---
+++ js/index.js
@@ -0,0 +1,244 @@
+$(function(){
+	
+
+	 var mySwiper = new Swiper ('.swiper-container', {
+		    direction: 'horizontal',
+		    // loop: true,
+		    // 点击跳转图片
+		 	paginationClickable :true,
+		    // 如果需要分页器
+		    pagination: '.swiper-pagination', 
+		    // 跳转时间、毫秒   
+		    autoplay:3000,
+		  })  
+	// header部分hover
+	$('#header').find('.header-ul li').hover(function(){
+		$(this).find('div').css('display','block')
+
+	},function(){
+		$(this).find('div').css('display','none')
+	});
+
+	// 城市列表hover
+	$('.header-city').hover(function(){
+		$(this).find('ul').css('display','block');
+	},function(){
+		$(this).find('ul').css('display','none')
+	})
+
+
+	// 导航hover
+	$('#nav .nav-list').hover(function(){
+		$(this).children('.list-div').css('display','block').siblings('a').children('i').css('display','block');
+	},function(){
+		$(this).children('div').css('display','none').siblings('a').children('i').css('display','none');
+	})
+	$('#nav').find('.nav-list').hover(function(){
+		$(this).children('a').css('background','#2b69aa');
+
+		$(this).has('.list-div').children('a').each(function(){
+			$(this).css('background','#2b69aa');
+			var thisa=$(this);
+			setTimeout(b,300);
+			function b(){
+				thisa.css('background','#0088d3');
+				// console.log(thisa);
+			}
+		})
+	},function(){
+		$(this).children('a').css('background','#0088d3')
+	})
+
+
+	$('#nav .nav-t-div').find('.ladn-register a').hover(function(){
+		$(this).css('text-decoration','underline');
+	},function(){
+		$(this).css('text-decoration','none');
+	})
+	$('#nav .nav-t-div').find('.mysjs a').hover(function(){
+		$(this).css('text-decoration','underline');
+	},function(){
+		$(this).css('text-decoration','none');
+	})
+	$('#nav .nav-t-div').find('.denglu-dingdan .dingdan').hover(function(){
+		$(this).css('text-decoration','underline');
+	},function(){
+		$(this).css('text-decoration','none');
+	})
+
+	$('#nav .nav-t-div').hover(function(){
+		$(this).find('.denglu-dingdan').css('display','block');
+	},function(){
+		$(this).find('.denglu-dingdan').css('display','none');
+	})
+
+
+	// 轮播图部分hover显示
+	$('#carousel-position .position-li').hover(function(){
+		$(this).children('a').css({'color':'#f39800'});
+		$(this).css({'background':'#fff','border-color':'#f39800'}).children('.apparatus').css('display','block').prevAll('.iconfont').css('color','#f39800');
+	},function(){
+		$(this).children('a').css({'color':'#ffffff'});
+		$(this).css({'background':'#0088d3','border-color':'#0088d3'}).children('.apparatus').css('display','none').prevAll('.iconfont').css('color','#fff');
+
+	}
+	);
+ 	
+ 	// table菜单
+ 	$('#content').find('.first-nav li').hover(function(){
+ 		$(this).css('color','#0088d3');
+ 	},function(){
+ 		$(this).css('color','#666');
+ 	})
+ 	$('#content').find('.first-nav li').on('click',function(){
+ 			var wh=$(this).width()/2-10+'px';
+		$(this).addClass('currency-all').find('i').css({
+			'left':wh,
+			'display':'block'
+		}).unbind('hover').parent('li').siblings('li').removeClass('currency-all').find('i').css('display','none');
+
+		var b =$(this).index();
+		$(this).parents('.tablemain').find('.parentdiv-div').eq(b).show().siblings().hide();
+		
+ 	});
+ 	// box-shadow
+ 	$('#activity-v').find('.activity-zhd li>div').hover(function(){
+ 		$(this).find('.activity-detail').addClass('boxshadow');
+ 	},function(){
+ 		$(this).find('.activity-detail').removeClass('boxshadow');
+ 	})
+ 	$('#train').find('li').hover(function(){
+ 		$(this).find('.train-v-mone').addClass('boxshadow');
+ 	},function(){
+ 		$(this).find('.train-v-mone').removeClass('boxshadow');
+ 	})
+	// 地区table菜单
+	$(".parentdiv-abc-li").hover(function(){
+		$(this).find('a').css({
+			'background':'#0088d3',
+			'color':'#fff',
+		})
+
+	},function(){
+		$(this).find('a').css({
+			'background':'none',
+			'color':'#0088d3',
+		})
+	})
+	$(".parentdiv-abc-li").on('click',function(){
+		var index=$(this).index();
+		$(this).parents('.parentdiv-div-city').find('.soujianshen-qb-quanbu').eq(index).css('display','block').siblings('.soujianshen-qb-quanbu').css('display','none');
+		$(this).addClass('cobfff').siblings('li').removeClass('cobfff');
+	})
+
+
+
+	// 移动定位
+	$(window).scroll(function(){
+		if($(document).scrollTop()>300){
+			$('#nav-left').css({
+				'position':'fixed',
+				'top':'200px',
+			})
+			$('#nav-left').find('li').eq('0').find('a').addClass('linkbp-bd').parents('li').siblings('li').find('a').removeClass('linkbp-bd');
+		}else{
+			if($('body').scrollTop()<350){
+				$("#nav-left").css({
+					'position':'absolute',
+					'top':'0px',
+				})
+			}
+		};
+		if($(document).scrollTop()>700){
+			$('#nav-left').find('li').eq('1').find('a').addClass('linkbp-bd').parents('li').siblings('li').find('a').removeClass('linkbp-bd');
+		}
+		if($(document).scrollTop()>1437){
+			$('#nav-left').find('li').eq('2').find('a').addClass('linkbp-bd').parents('li').siblings('li').find('a').removeClass('linkbp-bd');
+		}
+	
+		if($(document).scrollTop()>1813){
+			$('#nav-left').find('li').eq('3').find('a').addClass('linkbp-bd').parents('li').siblings('li').find('a').removeClass('linkbp-bd');
+		}
+		if($(document).scrollTop()>2242){
+			$('#nav-left').find('li').eq('4').find('a').addClass('linkbp-bd').parents('li').siblings('li').find('a').removeClass('linkbp-bd');
+		}
+		if($(document).scrollTop()>2800){
+			$('#nav-left').find('li').eq('5').find('a').addClass('linkbp-bd').parents('li').siblings('li').find('a').removeClass('linkbp-bd');
+		}
+		if($(document).scrollTop()>3200){
+			$('#nav-left').find('li').eq('6').find('a').addClass('linkbp-bd').parents('li').siblings('li').find('a').removeClass('linkbp-bd');
+		}
+	});
+	$('#nav-left').find('li').hover(function(){
+		$(this).find('a em').css('display','none');
+	},function(){
+		$(this).find('a em').css('display','block');
+	})
+
+	$('#nav-left').find('li').hover(function(){
+		$(this).find('.linkbp span').css({
+			'display':'block',
+			'background':'#0088d3',
+			'color':'#fff',
+		});
+		$(this).find('.linkbp em').css('display','none');
+		if($(this).find('a').hasClass('linkbp-bd')==false){
+			$(this).prev('li').find('a').css('border','none');
+		}
+	},function(){
+		$(this).prev('li').find('a').css('border-bottom','1px solid #8a8a8a');
+		$(this).find('.linkbp span').css('display','none');
+		$(this).find('.linkbp em').css('display','inline-block');
+	})
+	// 
+	$('#nav-left').find('li').on('click',function(){
+		$(this).prev('li').find('a').css('border-bottom','1px solid #8a8a8a');
+		$(this).siblings('li').find('a').removeClass('linkbp-bd');
+		$(this).find('a').addClass('linkbp-bd');
+		console.log('sss')
+	})
+	$('#nav-left').find('li').eq(0).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'513px',
+		},250);
+	})
+	$('#nav-left').find('li').eq(1).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'970px',
+		},250)
+	})
+	$('#nav-left').find('li').eq(2).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'1751px',
+		},250)
+	})
+	$('#nav-left').find('li').eq(3).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'2140px',
+		},250)
+	})
+	$('#nav-left').find('li').eq(4).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'2540px',
+		},250)
+	})
+	$('#nav-left').find('li').eq(5).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'3195px',
+		},250)
+	})
+	$('#nav-left').find('li').eq(6).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'3300px',
+		},250)
+	})
+
+	$('#nav-left').find('li').last().on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'0',
+		},250)
+	})
+
+
+
+})
---
+++ js/tuangou.js
@@ -0,0 +1,220 @@
+$(function(){
+	
+
+		// 切换城市
+	$('#dingwei').find('.dingwei-chengshi-c input').focus(function(){
+		$(this).css('font-size','18px');
+		$('#dingwei').find('.dingwei-city').css('display','block');
+		$(this).keydown(function(e){
+			e?event:window.event;
+			if(e.keyCode==13){
+				console.log('sss')
+			}
+		})
+	})
+	$('#dingwei').find('.dingwei-chengshi-c input').blur(function(){
+		$(this).css('font-size','14px');
+	})	
+	$(document).click(function(){
+			$('.dingwei-city').css('display','none');
+	})
+	$("#dingwei").find('.dingwei-chengshi').on('click',function(e){
+		e = e? event:window.event;
+		e.stopPropagation();
+		$('#dingwei').find('.dingwei-chengshi').css('border-color','#0088d3');
+		$('#dingwei').find('.dingwei-city').css('display','block');
+		$('#dingwei').find('.dingwei-chengshi-c input').css('font-size','18px');
+
+	})
+	$('#dingwei').find('.dingwei-city .dingwei-city-shuru span').on('click',function(e){
+		e?event:window.event;
+		e.stopPropagation();
+		$('.dingwei-city').css('display','none');
+	})
+	$('#dingwei').find('.dingwei-city .dingwei-city-csszm').find('.dingwei-city-hot li').on('click',function(e){
+		e?event:window.event;
+		e.stopPropagation();
+		$(this).addClass('hot_selected').siblings('li').removeClass('hot_selected');
+		var int=$(this).index();
+		console.log($(this).parents('.dingwei-city-csszm').find('.dingwei-city-chengshixq'));
+		$(this).parents('.dingwei-city-csszm').find('.dingwei-city-chengshixq').eq(int).removeClass('displaynone').siblings('.dingwei-city-chengshixq').addClass('displaynone');
+	})
+
+	// 健身nav
+	$('#nav').find('.nav-list').hover(
+		function(){
+			$(this).find('i').addClass('sanjiao').parents('.nav-list').siblings('li').find('i').removeClass('sanjiao');
+			$(this).find('.list-div').addClass('block').parents('.nav-list').siblings('li').find('.list-div').removeClass('block');
+		},function(){
+			$(this).parents('.nav-f-ul').find('.list-div').eq(1).addClass('block').parents('.nav-list').find('i').addClass('sanjiao').parents('.nav-list').siblings('.nav-list').find('.list-div').removeClass('block').parents('.nav-list').find('i').removeClass('sanjiao');
+		}
+	)
+	$('#nav .nav-t-div').find('.ladn-register a').hover(function(){
+		$(this).css('text-decoration','underline');
+	},function(){
+		$(this).css('text-decoration','none');
+	})
+	$('#nav .nav-t-div').find('.denglu-dingdan .dingdan').hover(function(){
+		$(this).css('text-decoration','underline');
+	},function(){
+		$(this).css('text-decoration','none');
+	})
+
+	$('#nav .nav-t-div').hover(function(){
+		$(this).find('.denglu-dingdan').css('display','block');
+	},function(){
+		$(this).find('.denglu-dingdan').css('display','none');
+	})
+
+
+
+
+	// 邮箱验证
+	$('#tg-jianshen').find('.ify-sousuo').find("input[type='submit']").on('click',function(){
+		
+		var emali =$(this).parents('.ify-sousuo').find("input[name='text']").val();
+		if(emali.length==0){
+			alert('邮箱不能为空');
+		}else{
+			var szReg =/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/ ;
+			var back = szReg.test(emali);
+			if(back==false){
+				alert('请检查输入的邮箱是否正确');
+			}
+			else{
+				return true;
+			}
+		}
+
+	})
+
+
+	// 团购分类hover
+	$('#categories').find('.categories-qjtgfl li').hover(
+		function(){
+			$(this).has('div').css('border-right','none').find(".tg-nav-submenu").css('display','block');
+			$(this).find('dl .tg-menu-arrow').css('display','none');
+		},function(){
+			$(this).find('dl .tg-menu-arrow').css('display','block');
+			$(this).css('border-right','1px solid #eaeaea').children(".tg-nav-submenu").css('display','none');
+		}
+	);
+	$('#categories').find('.categories-fl li').hover(function(){
+		$(this).css('background','#e95900');
+	},function(){
+		$(this).css('background','none');
+	})
+
+	$('#categories').find('.categories-fl li').on('click',function(){
+		$(this).addClass('categories-fl-fili').siblings('li').removeClass('categories-fl-fili');
+	})
+
+	
+	$('#tg-jianshen').find('.more').on('mouseover',function(){
+		$(this).find('.icon').css({
+			'transform':'rotate(-180deg)',
+		}).parents('.ify-hot-fl').addClass('on');
+		
+		$('.ify-hot-fl').on('mouseleave',function(){
+			$(this).removeClass('on');
+			$(this).find('.icon').css({
+				'transform':'rotate(0deg)',
+			})
+		})
+	})
+	$('#tg-jianshen').find('.ify-rot-sq li a').hover(function(){
+		$(this).css('color','#0088d3');
+	},function(){
+		$(this).css('color','#666');
+	})
+	$('#tg-jianshen').find('.categories-tgsy-ify').find('.lately>p a').on('click',function(){
+		$(this).parents('.lately').css('display','none');
+	})
+
+	// fixed定位
+
+	$(window).scroll(function(){
+		// console.log($('body').scrollTop());
+		if($(document).scrollTop()>1040){
+			$("#nav-fixed").css({
+				'position':'fixed',
+				'top':'0',
+			})
+			$('#nav-fixed').find('li').eq(0).addClass('nav-fixed-bac').siblings('li').removeClass('nav-fixed-bac');
+		}else{
+			if($(document).scrollTop()<1036){
+				$('#nav-fixed').css({
+					'position':'absolute',
+					'top':'0',
+				})
+
+			}
+		}
+		if($(document).scrollTop()>2000){
+			$('#nav-fixed').find('li').eq(1).addClass('nav-fixed-bac').siblings('li').removeClass('nav-fixed-bac');
+		}
+		if($(document).scrollTop()>2712){
+			$('#nav-fixed').find('li').eq(2).addClass('nav-fixed-bac').siblings('li').removeClass('nav-fixed-bac');
+		}
+		if($(document).scrollTop()>3443){
+			$('#nav-fixed').find('li').eq(3).addClass('nav-fixed-bac').siblings('li').removeClass('nav-fixed-bac');
+		}
+		if($(document).scrollTop()>4160){
+			$('#nav-fixed').find('li').eq(4).addClass('nav-fixed-bac').siblings('li').removeClass('nav-fixed-bac');
+		}
+		if($(document).scrollTop()>4877){
+			$('#nav-fixed').find('li').eq(5).addClass('nav-fixed-bac').siblings('li').removeClass('nav-fixed-bac');
+		}
+		if($(document).scrollTop()>5583){
+			$('#nav-fixed').find('li').eq(6).addClass('nav-fixed-bac').siblings('li').removeClass('nav-fixed-bac');
+		}
+		if($(document).scrollTop()>6326){
+			$('#nav-fixed').find('li').eq(7).addClass('nav-fixed-bac').siblings('li').removeClass('nav-fixed-bac');
+		}
+	})
+
+	$('#nav-fixed').find('li').eq(0).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'1043',
+		},250)
+	})
+	$('#nav-fixed').find('li').eq(1).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'2062',
+		},250)
+	})
+	$('#nav-fixed').find('li').eq(2).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'2778',
+		},250)
+	})
+	$('#nav-fixed').find('li').eq(3).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'3493',
+		},250)
+	})
+	$('#nav-fixed').find('li').eq(4).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'4210',
+		},250)
+	})
+	$('#nav-fixed').find('li').eq(5).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'4927',
+		},250)
+	})
+	$('#nav-fixed').find('li').eq(6).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'5643',
+		},250)
+	})
+	$('#nav-fixed').find('li').eq(7).on('click',function(){
+		$('body,html').animate({
+			'scrollTop':'6360',
+		},250)
+		console.log('sss')
+	})
+	$('#nav-fixed').find('li').last().on('click',function(){
+		$('body,html').scrollTop(0);
+	})
+})
